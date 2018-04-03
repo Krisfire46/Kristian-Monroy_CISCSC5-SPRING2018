c372817cd2b5b87d0f08725f5e6c789f0a098b5b
Krisfire46 <kmrtodayfire@gmail.com>
2018-04-03 12:47:03 -0700

    Signed-off-by: Krisfire46 <kmrtodayfire@gmail.com>

---
+++ Porject1/main.cpp
@@ -0,0 +1,32 @@
+/* 
+ * File:   main.cpp
+ * Author Kristian Monroy
+ * Created on April 3, 2018, 11:29 PM
+ * Purpose: Gaming making 
+ */
+
+//System Libraries
+#include <iostream>
+#include <>
+using namespace std;
+
+//User Libraries
+
+//Global Constants - Math/Physics Constants, Conversions,
+//                   2-D Array Dimensions
+
+//Function Prototypes
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+    //Declare Variables
+    
+    //Initialize Variables
+    
+    //Process/Map inputs to outputs
+    
+    //Output data
+    
+    //Exit stage right!
+    return 0;
+}
